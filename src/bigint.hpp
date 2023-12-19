3393df2782a3afa6a4c1e58d0b3cb9cc4f45328a
Tom Forest <65450199+tom-forest@users.noreply.github.com>
2023-12-19 16:55:42 +0100

    .gitignore added

--- src/bigint.hpp
+++ src/bigint.hpp
@@ -38,18 +38,30 @@ public:
 
 private:
     /**
-     * @brief Stores the value of the bigint object by slices of 18 digits. Inneficient but easy to implement.
+     * @brief Stores the value of bigint represented in base 2^64. Index 0 of the vector is the least significant component.
      * 
      */
     vector<uint64_t> values;
-
-    int max_digits = log10(UINT64_MAX) - 1;
 
 };
 
 bigint::bigint() : values({0}) {}
 
 bigint::bigint(uint64_t initial_value) : values({initial_value}) {}
+
+/**
+ * @brief Divides the provided number by 2^32 and returns the remainder.
+ * 
+ * @param number A digit string representing the number. Will be modified.
+ * @return uint64_t Remainder.
+ */
+static uint64_t string_euclid_32(string& number) {
+    size_t len = number.size();
+    uint64_t remainder = 0;
+    for (int i = 0; i < len; i++) {
+
+    }
+}
 
 bigint::bigint(string initial_values) {
     
---
+++ src/test.cpp
@@ -0,0 +1,7 @@
+#include <cstdint>
+#include <iostream>
+
+int main() {
+    uint64_t a = 1;
+    std::cout << a + UINT64_MAX << "\n";
+}

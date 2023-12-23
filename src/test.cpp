ae13f3063c45ea2808eba13c3cecc90e6a5e2b90
Tom Forest <65450199+tom-forest@users.noreply.github.com>
2023-12-23 20:40:16 +0100

    impl of to_string, now stuff works. Need to impl negatives.

--- src/test.cpp
+++ src/test.cpp
@@ -33,6 +33,23 @@ public:
      */
     bigint(const string& initial_value);
 
+    /**
+     * @brief Outputs the number in base 10.
+     * 
+     * @param os 
+     * @param number 
+     * @return ostream& 
+     */
+    friend ostream& operator<<(ostream& os, const bigint& number);
+
+
+    /**
+     * @brief Returns the number in base 10.
+     * 
+     * @return string 
+     */
+    string to_string() const;
+
 private:
     /**
      * @brief Stores the value of bigint represented in base 2^64. Index 0 of the vector is the least significant component.
@@ -40,17 +57,70 @@ private:
      */
     vector<uint64_t> values;
 
+    /**
+     * @brief Stores the sign of the bigint. True is positive. Because reasons.
+     * 
+     */
+    bool sign;
+
 };
 
 /**
- * @brief Constant used for division by 2^32. Bad practice but computing it everytime would be a waste.
+ * @brief Constant used for computations by blocs of ~32 bits. Bad practice but computing it everytime would be a waste.
  * 
  */
 static uint64_t STEP_32 = (uint64_t) log10(UINT32_MAX) - 1;
 
+
+/**
+ * @brief Power function that uses uint64_t variables internally. Does not check for overflow.
+ * 
+ * @param base 
+ * @param exp 
+ * @return uint64_t 
+ */
+static uint64_t pow_64(uint64_t base, uint64_t exp) {
+    uint64_t res = 1;
+    for (uint64_t i = 0; i < exp; i++) {
+        res *= base;
+    }
+    return res;
+}
+
+
+/**
+ * @brief Removes unnecessary 0s at the beginning of a string of digits representing a number.
+ * 
+ * @param number 
+ */
+static void truncate_0s(string& number) {
+    uint64_t l = number.length();
+    uint64_t i = 0;
+    while (number[i] == '0' and i < l) {
+        i ++;
+    }
+
+    number = number.substr(i, l);
+}
+
+
+/**
+ * @brief Adds unnecessary 0s at the beginning of a string of digits representing a number.
+ * 
+ * @param number String of digits to modify.
+ * @param n_0s Number of 0s to add.
+ */
+static void add_0s(string& number, uint64_t n_0s) {
+    for (uint64_t i = 0; i < n_0s; i++) {
+        number = "0" + number;
+    }
+}
+
+
 bigint::bigint() : values({0}) {}
 
 bigint::bigint(uint64_t initial_value) : values({initial_value}) {}
+
 
 /**
  * @brief Divides the provided number by 2^32 and returns the remainder.
@@ -80,7 +150,7 @@ static uint64_t string_euclid_32(const string& number, string& result) {
         */ 
         if ((i - j > STEP_32) or (i == len)) {
 
-            remainder *= (uint64_t) pow(10, i - j);
+            remainder *= pow_64(10, i - j);
             remainder += stoull(number.substr(j, i - j));
             string quotient = to_string(remainder / 0x100000000ULL);
 
@@ -88,9 +158,7 @@ static uint64_t string_euclid_32(const string& number, string& result) {
                 are needed for concatenation with the beginning of the quotient, so they have to be added
                 back.
             */
-            for (uint64_t k = j; k < i - quotient.length(); k++) {
-                quotient = "0" + quotient;
-            }
+            add_0s(quotient, i - quotient.length() - j);
 
             result += quotient;
             remainder %= 0x100000000ULL;
@@ -99,29 +167,151 @@ static uint64_t string_euclid_32(const string& number, string& result) {
         }
     }
 
-    uint64_t i = 0, result_size = result.length();
-
-    while (i < result_size and result[i] == '0') {
-        i += 1;
-    }
-
-    result = result.substr(i, result_size - i);
+    truncate_0s(result);
 
     return remainder;
 }
+
 
 /**
  * @brief Divides the provided number by 2^64 and returns the remainder.
  * 
  * @param number A digit string representing the input number.
  * @param result A digit string representing the result. Will be overwritten by the function.
- * @return uint64_t Remainder.
+ * @return uint64_t remainder of the euclidean division.
  */
 static uint64_t string_euclid_64(const string& number, string& result) {
     string quo32 = "";
     uint64_t mod32 = string_euclid_32(number, quo32);
     return 0x100000000ULL * string_euclid_32(quo32, result) + mod32;
 }
+
+/**
+ * @brief Retrieves a uint64_t from a string of digits. Returns 0 if pos is out of range.
+ * 
+ * @param number Source string.
+ * @param pos Position of the number in the string.
+ * @param len Number of digits to extract.
+ * @return uint64_t 
+ */
+static uint64_t string_to_64(const string& number, uint64_t pos, uint64_t len) {
+    if (len == 0) {
+        return 0;
+    }
+    if (pos < number.length()) {
+        return stoull(number.substr(pos, len));
+    }
+    return 0;
+}
+
+
+/**
+ * @brief   Adjust given index and reach to fit inside given length.
+ *          Used for exploring strings of digits from right to left by blocs.
+ * 
+ * @param i Index.
+ * @param b Reach.
+ * @param l Length.
+ */
+static void adjust_index(uint64_t& i, uint64_t& b, uint64_t l) {
+    if (i <= l) {
+            b = 2 * STEP_32;
+            i = l - i;
+        }
+        else if (i - l < 2 * STEP_32){
+            b = l % (2 * STEP_32);
+            i = 0;
+        }
+        else {
+            b = 0, i = 0;
+        }
+}
+
+
+static void string_add(const string& number1, const string& number2, string& result) {
+    result = "";
+    string temp;
+
+    uint64_t l1 = number1.length(), l2 = number2.length();
+    uint64_t long_length = max(l1, l2);
+    uint64_t numbuffer = 0, carry = 0, i = 0, bias = 0, i1, i2, b1, b2;
+
+    while (i < long_length or carry != 0) {
+        i += 2 * STEP_32;
+
+        i1 = i, i2 = i;
+
+        adjust_index(i1, b1, l1);
+        adjust_index(i2, b2, l2);
+
+        numbuffer = string_to_64(number1, i1, b1);
+        numbuffer += string_to_64(number2, i2, b2);
+        numbuffer += carry;
+
+        carry = numbuffer / pow_64(10, 2 * STEP_32);
+        numbuffer %= pow_64(10, 2 * STEP_32);
+
+        temp = to_string(numbuffer);
+        bias = temp.length();
+
+        add_0s(temp, 2 * STEP_32 - bias);
+
+        result = temp + result;
+    }
+
+    truncate_0s(result);
+}
+
+
+/**
+ * @brief Multiplies the number represented by a string of digits by 2^32.
+ * 
+ * @param number Number to be multiplied.
+ * @param result String to store the result.
+ */
+static void string_mult_32(const string& number, string& result) {
+    result = "";
+    uint64_t len = number.length();
+
+    uint64_t buffer_int;
+    string buffer_string, buffer_result;
+
+    for (uint64_t i = 0; i < len; i += STEP_32) {
+        buffer_int = string_to_64(number, i, STEP_32);
+        buffer_int *= 0x100000000ULL;
+
+        buffer_string = "";
+
+        uint64_t number_of_0s = len - i - STEP_32;
+
+        //  Overflow management.
+        if (number_of_0s > len) {
+            number_of_0s = 0;
+        }
+
+        add_0s(buffer_string, number_of_0s);
+
+        buffer_string = to_string(buffer_int) + buffer_string;
+        string_add(buffer_string, result, buffer_result);
+
+        result = buffer_result;
+    }
+} 
+
+
+/**
+ * @brief   Multiplies the number represented by a string of digits by 2^64.
+ *          Using blocs of 32 is necessary since 64 blocs would overflow.
+ * 
+ * @param number Number to be multiplied.
+ * @param result String to store the result.
+ */
+static void string_mult_64(const string& number, string& result) {
+    string buffer_string;
+    string_mult_32(number, buffer_string);
+    string_mult_32(buffer_string, result);
+}
+
 
 /**
  * @brief Checks wether a character is a digit or not.
@@ -152,10 +342,27 @@ bigint::bigint(const string& number_string) {
     }
 }
 
+
+string bigint::to_string() const{
+    string value_string = "";
+    string buffer_result;
+    uint64_t val_size = values.size();
+    for (uint64_t i = 0; i < values.size(); i++) {
+        string_mult_64(value_string, buffer_result);
+        value_string = buffer_result;
+
+        string_add(value_string, std::to_string(values[val_size - i - 1]), buffer_result);
+        value_string = buffer_result;
+    }
+    return value_string;
+}
+
+
+ostream& operator<<(ostream& os, const bigint& number) {
+    os << number.to_string();
+    return os;
+}
+
+
 int main() {
-    cout << (int) '0';
-    cout << "STEP32: " << STEP_32 << "\n";
-    string res = "";
-    uint64_t mod = string_euclid_64("18446744073709551615", res);
-    cout << res << " | " << mod << "\n";
 }
